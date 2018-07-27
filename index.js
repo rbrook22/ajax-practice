75d0afd90d9d91235130474bb4d734cf1e1e8fd8
Richard Brooks <rickbrooks22@gmail.com>
2018-07-27 06:46:21 -0400

    added helper function to pull in data and display on page

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    // console.log('State Changed:', xhr.readyState,xhr.status);
-    if(xhr.readyState == 4 && xhr.status == 200) {
-            var jsonData = JSON.parse(xhr.responseText);
-            console.log(jsonData);
-        }
-}
 
-xhr.open('GET',"https://randomuser.me/api/", true);
-xhr.send();
+var output = document.getElementById('output');
+var buttonClick = document.getElementById('loadNew');
+
+buttonClick.addEventListener('click', function(){
+    loadxhr();
+});
+
+function loadxhr(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        // console.log('State Changed:', xhr.readyState,xhr.status);
+        if(xhr.readyState == 4 && xhr.status == 200) {
+                var json = JSON.parse(xhr.responseText);
+                var data = json.results[0];
+                console.log(data);
+                var message = data.name.first + ' ' + data.name.last + '<br><img src="+data.picture.thumbnail +">'
+                output.innerHTML = message;
+
+
+            }
+    }
+    xhr.open('GET',"https://randomuser.me/api/", true);
+    xhr.send();
+}
